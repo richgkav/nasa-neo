8c5d0fb96137e576dad4aac9a40be1a949afc72d
Richard Kavanagh <richgkav@gmail.com>
2020-07-22 23:08:22 +0100

    added sorting

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ const main = function _main() {
 	//log({test1: 'test', test2: 'test2', test3: 'test3'});
 	//log(createQuery('2020-07-01','2020-07-07'))
 	
-	const dateFrom = '2020-07-13';
-	const dateTo = '2020-07-19';
+	const dateFrom = '2020-07-01';
+	const dateTo = '2020-07-07';
+	let asteroidsArray = [];
 
-	document.getElementById('title').innerText = `Close Approaches ${dateFrom} to ${dateTo}`;
+	document.getElementById('title').innerText = 
+		`Near Earth Object - Asteroid Approaches - ${dateFrom} to ${dateTo}`;
 	
 	const fetchQuery = createQuery(dateFrom, dateTo);
 	
@@ -26,16 +28,102 @@ const main = function _main() {
 		return response.json();
 	})
 	.then(function(response) {
-		
-		//log(response.element_count.toString() + ' asteroids');
-		
-		const responseObject = response.near_earth_objects;
-		log(responseObject);
-		const asteroidsArray = fillAsteroidsArray(responseObject);
-		//sortArray(asteroidsArray, 'date');
-		sortArray(asteroidsArray, 'diameter');
-		renderData(asteroidsArray);
+
+		log(response);
+		asteroidsArray = fillAsteroidsArray(response);
+		renderAsteroidData(asteroidsArray);
+		log(typeof asteroidsArray[0].closeApproachVelocity);
 	});
+
+	// true doesn't mean a specific direction it's just a flip-flop flag
+	let sortDirection = {
+		name: true,
+		diameter: true,
+		hazard: true,
+		date: true,
+		distance: true,
+		velocity: true,
+	};
+
+	addSortClickEvents();
+
+	function addSortClickEvents() {
+
+		document.getElementById('ast-name').addEventListener('click', () => {
+			//log('name clicked');
+
+			if (sortDirection.name === true) {
+				sortArray(asteroidsArray, 'name', true);
+				sortDirection.name = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'name', false);
+				sortDirection.name = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+		
+		document.getElementById('ast-diameter').addEventListener('click', () => {
+			if (sortDirection.diameter === true) {
+				sortArray(asteroidsArray, 'diameter', true);
+				sortDirection.diameter = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'diameter', false);
+				sortDirection.diameter = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+
+		document.getElementById('ast-hazard').addEventListener('click', () => {
+			if (sortDirection.potentialHazard === true) {
+				sortArray(asteroidsArray, 'potentialHazard', true);
+				sortDirection.potentialHazard = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'potentialHazard', false);
+				sortDirection.potentialHazard = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+		
+		document.getElementById('ast-date').addEventListener('click', () => {
+			if (sortDirection.date === true) {
+				sortArray(asteroidsArray, 'date', true);
+				sortDirection.date = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'date', false);
+				sortDirection.date = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+
+		document.getElementById('ast-distance').addEventListener('click', () => {
+			if (sortDirection.closeApproachDistance === true) {
+				sortArray(asteroidsArray, 'closeApproachDistance', true);
+				sortDirection.closeApproachDistance = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'closeApproachDistance', false);
+				sortDirection.closeApproachDistance = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+
+		document.getElementById('ast-velocity').addEventListener('click', () => {
+			if (sortDirection.closeApproachVelocity === true) {
+				sortArray(asteroidsArray, 'closeApproachVelocity', true);
+				sortDirection.closeApproachVelocity = false;	// flip the direction for next sort
+			}
+			else {
+				sortArray(asteroidsArray, 'closeApproachVelocity', false);
+				sortDirection.closeApproachVelocity = true;
+			}
+			renderAsteroidData(asteroidsArray);
+		});
+
+	}
 	
 	function createQuery(start, end) {
 		const url = 'https://api.nasa.gov/neo/rest/v1/feed?';
@@ -43,58 +131,116 @@ const main = function _main() {
 		return url+'start_date='+start+'&end_date='+end+'&api_key='+key;
 	}
 	
-	function fillAsteroidsArray(responseObject) {
+	function fillAsteroidsArray(response) {
 		
-		const asteroids = [];
+		const asteroidsArray = [];
 
-		for (const date in responseObject) {
+		// response object configuration
+		/* 
+		{
+			'links':{},
+			'element_count': number,
+			'near_earth_objects': 
+			{
+				'date': 
+				[
+					{ asteroid },
+					{ asteroid },
+					...
+				]
+				'date': []
+				...
+			}
+		}
+		*/
 
-			const dateData = responseObject[date];
+		const neoObject = response.near_earth_objects;
 
-			for (const astData in dateData) {
+		for (const eachDateArray in neoObject) {
 
-				const astDataObj = dateData[astData];
+			// get reference to the array of asteroids
+			const asteroidDataArray = neoObject[eachDateArray];
+
+			// iterate through each asteroid object in the array
+			for (const singleAsteroidData in asteroidDataArray) {
+
+				// get reference to asteroid
+				const asteroidObject = asteroidDataArray[singleAsteroidData];
+
+				// this just makes it a bit tidier
+				const closeApproachData = asteroidObject.close_approach_data[0];
+
+				// Creating an Asteroid object to make rendering easier.
 				const asteroid = new Asteroid(
-					astDataObj.name,
-					avg(astDataObj.estimated_diameter.meters),
-					astDataObj.is_potentially_hazardous_asteroid,
-					astDataObj.nasa_jpl_url,
-					astDataObj.close_approach_data[0].close_approach_date_full,
-					twoDecPlaces(astDataObj.close_approach_data[0].miss_distance.kilometers),
-					twoDecPlaces(astDataObj.close_approach_data[0].relative_velocity.kilometers_per_second)
+					asteroidObject.name,
+					avg(asteroidObject.estimated_diameter.meters),
+					asteroidObject.is_potentially_hazardous_asteroid,
+					asteroidObject.nasa_jpl_url,
+					closeApproachData.close_approach_date_full,
+					twoDecPlaces(closeApproachData.miss_distance.kilometers),
+					twoDecPlaces(closeApproachData.relative_velocity.kilometers_per_second)
 				);
-				asteroids.push(asteroid);
+				asteroidsArray.push(asteroid);
 				
 			}
 		}
-		return asteroids;
+		return asteroidsArray;
 	}
 	
-	function sortArray(array, property) {
-		
-		array.sort(function(a,b) {
-			//return a.date - b.date;
-			return a[property] - b[property];
-		});
-
+	function sortArray(array, property, direction) {
+		if (typeof property === 'number') {
+			if (direction) {
+				log('number');
+				array.sort(function(a, b) {
+					return a[property] - b[property];
+				});
+			}
+			else {
+				log('number');
+				array.sort(function(a, b) {
+					return b[property] - a[property];
+				});		
+			}
+		}
+		else if (typeof property === 'string') {
+			if (direction) {
+				log('string');
+				array.sort(function(a, b) {
+					if (a[property] < b[property]) return -1;
+					if (a[property] > b[property]) return 1;
+					return 0;
+				});
+			}
+			else {
+				log('string');	
+				array.sort(function(a, b) {
+					if (a[property] < b[property]) return 1;
+					if (a[property] > b[property]) return -1;
+					return 0;
+				});
+			}
+		}
 	}
 
-	function renderData(asteroids) {
+	function renderAsteroidData(asteroidsArray) {
 
+		log('renderAsteroidData called');
 		const content = getContent();
+		clearChildElements(content);
 
-		asteroids.forEach(asteroid => {
+		asteroidsArray.forEach(asteroid => {
 
 			const asteroidDiv = newDiv('asteroid');
 			content.appendChild(asteroidDiv);
 
-			asteroidDiv.appendChild(renderAsteroidDataBox('Name', asteroid.name));
-			asteroidDiv.appendChild(renderAsteroidDataBox('Diameter (meters)', asteroid.diameter));
-			asteroidDiv.appendChild(renderAsteroidDataBox('Hazard?', asteroid.potentialHazard));
-			asteroidDiv.appendChild(renderAsteroidLink('URL', asteroid.urlNasa));
-			asteroidDiv.appendChild(renderAsteroidDataBox('Date', asteroid.closeApproachDateFull));
-			asteroidDiv.appendChild(renderAsteroidDataBox('Distance (km)', asteroid.closeApproachDistance));
-			asteroidDiv.appendChild(renderAsteroidDataBox('Velocity (m/s)', asteroid.closeApproachVelocity));
+			// string in function call is used for 
+			asteroidDiv.appendChild(renderAsteroidDataBox('name', asteroid.name));
+			asteroidDiv.appendChild(renderAsteroidDataBox('diameter', asteroid.diameter));
+			asteroidDiv.appendChild(renderAsteroidDataBox('hazard', asteroid.potentialHazard));
+			asteroidDiv.appendChild(renderAsteroidLink('url', asteroid.urlNasa));
+			asteroidDiv.appendChild(renderAsteroidDataBox('date', asteroid.closeApproachDateFull));
+			asteroidDiv.appendChild(renderAsteroidDataBox('distance', asteroid.closeApproachDistance));
+			asteroidDiv.appendChild(renderAsteroidDataBox('velocity', asteroid.closeApproachVelocity));
 
 		});
 	}
@@ -102,19 +248,21 @@ const main = function _main() {
 	// ---------------------------------------------------------------------------//
 	
 	function renderAsteroidDataBox(title, asteroidData) {
+
 		const divs = renderDivs(title, asteroidData);
+
 		divs.asteroidDataDiv.innerText = asteroidData;
 
-		if (title === "Hazard?" && asteroidData === true) {
+		if (title === "hazard" && asteroidData === true) {
 			divs.asteroidDataBox.classList.add('cell-highlight');
 		}
-		if (title === "Distance (km)" && asteroidData < 384400) {
+		else if (title === "distance" && asteroidData < 384400) {
 			divs.asteroidDataBox.classList.add('cell-highlight');
 		}
-		if (title === "Diameter (meters)" && asteroidData > 1000) {
+		else if (title === "diameter" && asteroidData > 1000) {
 			divs.asteroidDataBox.classList.add('cell-highlight');
 		}
-		if (title === "Velocity (m/s)" && asteroidData > 25) {
+		else if (title === "velocity" && asteroidData > 25) {
 			divs.asteroidDataBox.classList.add('cell-highlight');
 		}
 
@@ -130,13 +278,17 @@ const main = function _main() {
 	function renderDivs(title, asteroidData) {
 		const asteroidDataBox = newDiv('asteroid-databox')
 		asteroidDataBox.classList.add('cell-colour');
-		const asteroidTitleDiv = newDiv('asteroid-title');
-		asteroidTitleDiv.innerText = title;
 		const asteroidDataDiv = newDiv('asteroid-data');
-		asteroidDataBox.appendChild(asteroidTitleDiv);
 		asteroidDataBox.appendChild(asteroidDataDiv);
-		return {asteroidDataBox, asteroidTitleDiv, asteroidDataDiv};
+		return {asteroidDataBox, asteroidDataDiv};
 	}
+
+	const clearChildElements = function _clearChildElements(node) {
+        // clears all child elements under the specified id
+        while (node.firstChild) {
+            node.removeChild(node.lastChild);
+        }
+    }
 // ---------------------------------------------------------------------------//
 	
 	// log('average test = ' + avg({ '1':10, '2':20, '3':30 }));
@@ -151,11 +303,10 @@ const main = function _main() {
 			count ++;
 		}
 		return twoDecPlaces(total/count);
-		//return total / count;
 	}
 	
 	function twoDecPlaces(value) {
-		return (Math.round((value)* 100)/100).toFixed(2);
+		return parseFloat((Math.round((value)* 100)/100).toFixed(2));
 	}
 
 	function newDiv(className) {
@@ -185,9 +336,9 @@ const main = function _main() {
 	}
 	
 	function log(obj) {
-		if (typeof obj === 'string')
-			getLog().innerText += obj + '\n';
-		else
+	//	if (typeof obj === 'string')
+	//		getLog().innerText += obj + '\n';
+	//	else
 			console.log(obj);
 	}
 
